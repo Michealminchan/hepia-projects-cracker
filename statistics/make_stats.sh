7ce6a4bf729c24e2dd6f51c99174c25831c5a177
claudio <claudio.sousa@epfl.ch>
2016-10-19 15:58:59 +0200

    data calculated

--- statistics/make_stats.sh
+++ statistics/make_stats.sh
@@ -3,7 +3,7 @@ do
     let threads=$(echo 2^$threadspow|bc)
     FILE="res_${threads}.out"
     rm $FILE
-    for i in {1..5}; 
+    for i in {1..20}; 
     do
         ./main NO_HASH 12 $threads>>$FILE
     done
